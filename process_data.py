788b258b2e28bfb703cca16f58f90be9e7c89b81
amosmim <amosmim@gmail.com>
2018-01-10 01:51:30 +0200

    process data

--- process_data.py
+++ process_data.py
@@ -1,6 +1,6 @@
 from sys import argv
 from eval import annon_to_dict as extractor
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 import spacy
 
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     annotation_file_name = argv[1]
     processed_file_name = argv[2]
     dont_care, annotation_data = extractor(annotation_file_name)
-    obj1 = defaultdict(set)
-    obj2 = defaultdict(set)
+    obj1 = defaultdict(Counter)
+    obj2 = defaultdict(Counter)
     with open(processed_file_name, "r") as f:
         corpus = f.readlines()
         i = 0
@@ -29,30 +29,30 @@ if __name__ == '__main__':
                     sent = nlp(unicode(corpus[i][7:]))
                     for ent in sent.ents:
                         if ent.text in obj1Targets:
-                            obj1['entities type'].add(ent.label_)
+                            obj1['entities type'][ent.label_] +=1
                         if ent.text in obj2Targets:
-                            obj2['entities type'].add(ent.label_)
+                            obj2['entities type'][ent.label_] +=1
 
                     i += 1
                     while corpus[i] != '\n':
                         parts = corpus[i].split('\t')
                         for target in obj1Targets:
                             if parts[1] in target.split():
-                                obj1['field 3'].add(parts[3])
-                                obj1['field 4'].add(parts[4])
-                                obj1['field 6'].add(parts[6])
-                                obj1['field 7'].add(parts[7])
-                                obj1['field 8'].add(parts[8].strip())
+                                obj1['field 3'][parts[3]] +=1
+                                obj1['field 4'][parts[4]] +=1
+                                obj1['field 6'][parts[6]] +=1
+                                obj1['field 7'][parts[7]] +=1
+                                obj1['field 8'][parts[8].strip()] +=1
                                 if parts[8].strip() == '':
                                     print "Null 8 field: " + target + " => "+ str(parts)
 
                         for target in obj2Targets:
                             if parts[1] in target.split():
-                                obj2['field 3'].add(parts[3])
-                                obj2['field 4'].add(parts[4])
-                                obj2['field 6'].add(parts[6])
-                                obj2['field 7'].add(parts[7])
-                                obj2['field 8'].add(parts[8].strip())
+                                obj2['field 3'][parts[3]] +=1
+                                obj2['field 4'][parts[4]] +=1
+                                obj2['field 6'][parts[6]] +=1
+                                obj2['field 7'][parts[7]] +=1
+                                obj2['field 8'][parts[8].strip()] +=1
                                 if parts[8].strip() == '':
                                     print "Null 8 field: "+ target + " => "+ str(parts)
                                 elif parts[8] == "PERSON":
@@ -66,14 +66,14 @@ if __name__ == '__main__':
         out.write("Object 1:\n")
         for field, ls in sorted(obj1.items()):
             out.write("\t" +field + "\n")
-            for type in  ls:
-                out.write("\t\t" + type + "\n")
+            for type , count in  ls.items():
+                out.write("\t\t" + type + "="+ str(count) +"\n")
             out.write("\n")
         out.write("\n\nObject 2:\n")
         for field, ls in sorted(obj2.items()):
             out.write("\t" +field + "\n")
-            for type in  ls:
-                out.write("\t\t" + type + "\n")
+            for type, count  in  ls.items():
+                out.write("\t\t" + type + "="+ str(count) +"\n")
             out.write("\n")
         out.close()
         #print ("obj 1 : " + str(obj1))
