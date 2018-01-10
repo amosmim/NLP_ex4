b327f578ab805267f578c1a42c769bf855fe18c0
amosmim <amosmim@gmail.com>
2018-01-10 10:43:07 +0200

    FN & FP prints in eval

--- eval.py
+++ eval.py
@@ -24,6 +24,8 @@ def annon_to_dict(file_name):
 if __name__ == '__main__':
     gold_file_name = sys.argv[1]
     predict_file_name = sys.argv[2]
+    false_positive = []
+    false_negative = []
 
     print 'Start read annotations files'
     gold_segments, gold_data = annon_to_dict(gold_file_name)
@@ -36,6 +38,12 @@ if __name__ == '__main__':
         for relation in gold_data[sentence_num]:
             if relation in predict_data[sentence_num]:
                 good_predicts += 1
+            else:
+                false_negative.append((sentence_num, ) + relation)
+        for relation in predict_data[sentence_num]:
+            if relation not in gold_data[sentence_num]:
+                false_positive.append((sentence_num, ) + relation)
+
     print 'Finished compare the data'
 
     precision = good_predicts / all_predicts
@@ -44,4 +52,19 @@ if __name__ == '__main__':
     f1 = (precision * recall) * 2
     if f1 != 0.0:
         f1 /= precision + recall
-    print ('precision: ' + str(precision) + '\tRecall: ' + str(recall) + '\tF1: ' + str(f1))
+    # Calculations tests
+    assert (len(false_positive) + good_predicts != all_predicts), "calculate error No.1"
+    assert (good_predicts - len(false_negative) + len(false_positive)  != gold_segments), "calculate error No.2"
+    assert (gold_segments + len(false_negative) - len(false_positive)  != gold_segments), "calculate error No.3"
+
+    # print results
+    print ('\nPrecision: ' + str(precision) + '\tRecall: ' + str(recall) + '\tF1: ' + str(f1))
+
+    print ("\nFalse Negative:")
+    for fn in false_negative:
+        print "\tsentence No.{0}:\tobj1 {1},\tobj2 {2}".format(fn[0], fn[1],fn[2])
+
+    print ("\n\nFalse Positive:")
+    for fp in false_negative:
+        print "\tsentence No.{0}:\tobj1 {1},\tobj2 {2}".format(fp[0], fp[1], fp[2])
+
